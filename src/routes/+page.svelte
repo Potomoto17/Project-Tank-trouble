fee9a007ec2e548dc944ddaa491b9f2f86daff79
GalPovsod8 <galpos10@gmail.com>
2025-03-27 22:34:24 +0100

    added aditional input implementation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
-<section class="bgImg bg-dabBrown grid h-screen min-h-max place-items-center bg-cover">
-	<form action="" class="bg-dabBrown border-blueNyanza flex flex-col rounded-2xl border-2 p-8">
+<section class="bgImg bg-dabBrown grid h-screen min-h-max place-items-center bg-cover p-[5vw]">
+	<form
+		action=""
+		class="bg-dabBrown border-blueNyanza flex w-full flex-col rounded-2xl border-2 p-8 md:w-max"
+	>
 		<div class="flex flex-col items-start justify-center gap-1">
 			<label for="username" class="text-blueNyanza font-bold">Username</label>
 			<input
-				class="bg-blueNyanza rounded-2xl border-2 border-black p-2 text-black"
+				class="bg-blueNyanza w-full rounded-2xl border-2 border-black p-2 text-black md:w-72"
 				type="text"
 				id="username"
 				name="username"
