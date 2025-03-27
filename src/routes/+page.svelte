bbd7aa1e9563edbc72ab81c5e13baa318a0e711f
GalPovsod8 <galpos10@gmail.com>
2025-03-27 22:29:12 +0100

    basic input implementation for registration form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,17 @@
-<section class="bgImg h-screen min-h-max bg-cover"></section>
+<section class="bgImg bg-dabBrown grid h-screen min-h-max place-items-center bg-cover">
+	<form action="" class="bg-dabBrown border-blueNyanza flex flex-col rounded-2xl border-2 p-8">
+		<div class="flex flex-col items-start justify-center gap-1">
+			<label for="username" class="text-blueNyanza font-bold">Username</label>
+			<input
+				class="bg-blueNyanza rounded-2xl border-2 border-black p-2 text-black"
+				type="text"
+				id="username"
+				name="username"
+				placeholder="Username"
+			/>
+		</div>
+	</form>
+</section>
 
 <style>
 	.bgImg {
