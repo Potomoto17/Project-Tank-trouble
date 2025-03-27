f13a0e4791955651846ede4a7b2565976d5a2f1c
GalPovsod8 <galpos10@gmail.com>
2025-03-28 00:12:54 +0100

    moved register to own place

---
+++ src/routes/(UI)/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<header
+	class="bg-dabBrown font-workSans text-blueNyanza absolute top-0 left-0 z-10 flex w-full items-center justify-between px-[5vw] py-4"
+>
+	<a href="/" class="text-2xl font-bold italic duration-150 ease-in-out hover:opacity-80"
+		>TankTrouble</a
+	>
+	<nav class="flex items-center gap-4 text-lg">
+		<a href="/" class="transition-all duration-150 ease-in-out hover:opacity-80">Leaderboard</a>
+		<a href="/profile" class="transition-all duration-150 ease-in-out hover:opacity-80">Profile</a>
+	</nav>
+</header>
+<main class="font-workSans">
+	{@render children()}
+</main>
--- src/routes/register/+page.svelte
+++
@@ -1,4 +0,0 @@
-<section>
-	<h1>Register</h1>
-	<p>Register page</p>
-</section>
