b9ff8479d6a97dcece772a28dfdfab5d4db758a3
GalPovsod8 <galpos10@gmail.com>
2025-03-31 18:03:30 +0200

    desing additions + icons

--- src/routes/(GAME)/game/+page.svelte
+++ src/routes/(GAME)/game/+page.svelte
@@ -1,19 +1,59 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-
 	let { data }: { data: PageData } = $props();
+
+	let isAsideOpen = $state(true);
+
+	function toggleAside() {
+		isAsideOpen = !isAsideOpen;
+	}
 </script>
 
 <section class="gameBg bg-blueNyanza flex h-screen min-h-screen">
-	<aside class="bg-dabBrown2 h-full w-[25vw]">aside</aside>
-	<div class="border-hunterGreen relative h-full w-[75vw] border-4">
-		<header class="absolute m-4 flex w-full gap-4 text-xl font-bold text-white">
-			<div class="bg-hunterGreen flex w-full items-center justify-between rounded-2xl p-4">
-				<p>PLAYER 1</p>
-				<p>2 : 3</p>
-				<p>PLAYER 2</p>
+	<aside
+		class="bg-dabBrown2 text-blueNyanza h-full overflow-hidden py-8 font-bold transition-all duration-150 ease-in-out"
+		class:w-[0vw]={!isAsideOpen}
+		class:w-[25vw]={isAsideOpen}
+		class:opacity-0={!isAsideOpen}
+		class:opacity-100={isAsideOpen}
+		class:p-8={isAsideOpen}
+		class:py-0={!isAsideOpen}
+	>
+		<div class="flex justify-between text-2xl">
+			<h1>Side Menu</h1>
+			<button onclick={toggleAside} class="cursor-pointer">&RightArrowLeftArrow;</button>
+		</div>
+	</aside>
+
+	<button
+		onclick={toggleAside}
+		class:opacity-0={isAsideOpen}
+		class="bg-hunterGreen hover:bg-opacity-90 absolute top-6 left-0 z-10 cursor-pointer rounded-r-lg p-2 text-2xl text-white transition-all duration-150"
+	>
+		&RightArrowLeftArrow;
+	</button>
+
+	<div
+		class="border-hunterGreen relative h-full border-4 transition-all duration-150"
+		class:w-[100vw]={!isAsideOpen}
+		class:w-[75vw]={isAsideOpen}
+	>
+		<header class="absolute top-0 right-0 left-0 p-4">
+			<div class="flex gap-4 text-xl font-bold text-white">
+				<div class="bg-hunterGreen flex flex-1 items-center justify-between gap-2 rounded-2xl p-4">
+					<p>PLAYER 1</p>
+					<p>2 : 3</p>
+					<p>PLAYER 2</p>
+				</div>
+				<div class="bg-hunterGreen flex w-max items-center gap-2 rounded-2xl p-4">
+					<a class="transition-all duration-150 ease-in-out hover:opacity-80" href="/profile"
+						><img class="h-6 w-6" src="/src/lib/assets/ui/icons/ic_profile.png" alt="Profile" /></a
+					>
+					<a class="transition-all duration-150 ease-in-out hover:opacity-80" href="/pause"
+						><img class="h-8 w-8" src="/src/lib/assets/ui/icons/ic_pause.png" alt="Pause" /></a
+					>
+				</div>
 			</div>
-			<div class="bg-hunterGreen w-max rounded-2xl p-4">pausemenu</div>
 		</header>
 		<div class="mainUiasdasdasdasdasd"></div>
 	</div>
